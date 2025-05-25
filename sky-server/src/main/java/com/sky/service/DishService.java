5aef51eb3d4c53889182733c126be4397e951a53
Laomolearncode <3066475098@qq.com>
2025-05-25 15:59:28 +0800

    修改菜品业务代码开发

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -4,6 +4,7 @@ import com.sky.dto.DishDTO;
 import com.sky.dto.DishPageQueryDTO;
 import com.sky.entity.Dish;
 import com.sky.result.PageResult;
+import com.sky.vo.DishVO;
 
 import java.util.List;
 
@@ -27,4 +28,18 @@ public interface DishService {
      * @return
      */
     void deleteBatch(List<Long> ids);
+
+    /**
+     * 根据id查询菜品和对应的口味数据
+     * @param id
+     * @return
+     */
+    DishVO getByIdWithFlavor(Long id);
+
+    /**
+     * 修改菜品
+     * @param dishDTO
+     * @return
+     */
+    void updateWithFlavor(DishDTO dishDTO);
 }
